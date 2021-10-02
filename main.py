da9a2b0c602ac1d9164b878c5ca78109003d7329
haok1402 <haok1402@gmail.com>
2021-10-02 07:36:42 -0400

    ✔ ADD; DEL; MOD on Server-Side

--- main.py
+++ main.py
@@ -6,23 +6,76 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     with open("static/nodeMap.html", "r") as f:
         return f.read()
 
 
 @app.route("/write", methods=["POST"])
 def writeFile():
-    # with open('/data/test.json', 'w') as f:
-    #     json.dump(request.data, f)
-    data = json.loads(request.data.decode('utf-8'))
-    with open("./data/temp.json", "a") as f:
-        json.dump(data, f, indent=4)
-        f.write("\n")
-    return ""
+    writeCommand = json.loads(request.data)
+    writeOperation = writeCommand.get('operation')
+    writeProperty = writeCommand.get('property')
+    writeValue = writeCommand.get('value')
+    fileID = writeValue.get('id')
+
+    if writeOperation == "ADD":
+        with open("./data/sample.json", "r") as f:
+            data = json.load(f)
+        f.close()
+        if writeProperty == 'node':
+            nodeLst = data.get("nodes")
+            nodeLst.append(writeValue)
+            data["nodes"] = nodeLst
+        elif writeProperty == 'edge':
+            nodeLst = data.get("edges")
+            nodeLst.append(writeValue)
+            data["edges"] = nodeLst
+        with open("./data/sample.json", "w") as f:
+            json.dump(data, f)
+        f.close()
+
+    elif writeOperation == "DEL":
+        with open("./data/sample.json", "r") as f:
+            data = json.load(f)
+        f.close()
+        if writeProperty == 'node':
+            nodeLst = data.get("nodes")
+            nodeLst.remove(writeValue)
+        elif writeProperty == 'edge':
+            nodeLst = data.get("edges")
+            nodeLst.remove(writeValue)
+        with open("./data/sample.json", "w") as f:
+            json.dump(data, f)
+        f.close()
+
+    elif writeOperation == "MOD":
+        with open("./data/sample.json", "r") as f:
+            data = json.load(f)
+        f.close()
+        if writeProperty == 'node':
+            nodeLst = data.get("nodes")
+            for i in range(len(nodeLst)):
+                if nodeLst[i].get('id') == fileID:
+                    nodeLst.pop(i)
+                    nodeLst.append(writeValue)
+            data["nodes"] = nodeLst
+        elif writeProperty == 'edge':
+            nodeLst = data.get("edges")
+            for i in range(len(nodeLst)):
+                if nodeLst[i].get('id') == fileID:
+                    nodeLst.pop(i)
+                    nodeLst.append(writeValue)
+            data["edges"] = nodeLst
+        with open("./data/sample.json", "w") as f:
+            json.dump(data, f)
+        f.close()
+
+    with open("static/nodeMap.html", "r") as f:
+        return f.read()
 
 
-@app.route("/read/<fileName>", methods=["GET"])
-def readFile(fileName):
-    with open(f'./data/{fileName}.json', 'r') as f:
+@app.route("/read/<fileID>", methods=["GET"])
+def readFile(fileID):
+    with open(f'./data/{fileID}.json', 'r') as f:
         return f.read()
