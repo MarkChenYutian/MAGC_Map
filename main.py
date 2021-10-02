ceffc8df6d3c3c183449dcf35bd305eab58e8fca
haok1402 <haok1402@gmail.com>
2021-10-02 02:56:51 -0400

    ✔ Create a Server with Flask ✔ Rearrange Directory

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    with open("static/nodeMap.html", "r") as f:
+        return f.read()
+
+
+@app.route("/write", methods=["POST"])
+def writeFile():
+    # with open('/data/test.json', 'w') as f:
+    #     json.dump(request.data, f)
+    print(json.dumps(request.data.decode('utf-8')))
+    return "static/nodeMap.html"
